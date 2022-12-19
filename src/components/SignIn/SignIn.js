d7187466b0e7b345d6c9a991507cf72fe5bdbb9a
Shimrit breef Ziskand <shimrit021@gmail.com>
2022-12-19 10:31:19 +0000

    search form mobile

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -69,42 +69,4 @@ function SignIn({
   );
 }
 
-//   return (
-//     <div className="sign-in">
-//       <PopupWithForm
-//         name={"sign-in"}
-//         isPopupOpen={isPopupOpen}
-//         onClose={onClose}
-//       >
-//         <form onSubmit={handleSubmit} className="form signin__form">
-//           <div className="signin">
-//             <h2 className="signin__welcome">Sign in</h2>
-//             <label htmlFor="email">Email</label>
-//             <input placeholder="email" required className="email" />
-//             <span className="popup__input-error"></span>
-//             <label htmlFor="password">Password</label>
-//             <input placeholder="password" required className="password" />
-//             <span className="popup__input-error"></span>
-//           </div>
-//           <div className="signin__button-container">
-//             <button type="submit" className="signin__button">
-//               Sign in
-//             </button>
-//           </div>
-//           <p className="popup__redirect">
-//             or
-//             <button
-//               type="button"
-//               className="signin__signup-link"
-//               onClick={handleSignUpButtonClick}
-//             >
-//               Sign up
-//             </button>
-//           </p>
-//         </form>
-//       </PopupWithForm>
-//     </div>
-//   );
-// }
-
 export default SignIn;
